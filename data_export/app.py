4ccd679d614314688f978c55924f8f2c4fb3e77d
lh220f <lhartman1@wayfair.com>
2023-12-13 15:08:31 -0500

    remove

--- data_export/app.py
+++ data_export/app.py
@@ -4,17 +4,17 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 def run():
-    uri = "mongodb+srv://tea_app:GQzBeQxpwlW3t4mV@cluster0.1buu3qz.mongodb.net/?retryWrites=true&w=majority"
+    # uri = "mongodb+srv://<>@cluster0.1buu3qz.mongodb.net/?retryWrites=true&w=majority"
 
-    # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'))
+    # # Create a new client and connect to the server
+    # client = MongoClient(uri, server_api=ServerApi('1'))
 
-    # Send a ping to confirm a successful connection
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        print(e)
+    # # Send a ping to confirm a successful connection
+    # try:
+    #     client.admin.command('ping')
+    #     print("Pinged your deployment. You successfully connected to MongoDB!")
+    # except Exception as e:
+    #     print(e)
 
     # quests.dump_text_file()
     # cutscenes.dump_text_file()
