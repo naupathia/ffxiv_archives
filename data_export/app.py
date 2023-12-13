8828ffbcb4f17d93ab5420e19b467ac69c647a31
lh220f <lhartman1@wayfair.com>
2023-12-13 14:41:16 -0500

    new user

--- data_export/app.py
+++ data_export/app.py
@@ -1,25 +1,27 @@
 from .parse import quests, cutscenes, items, tripletriadcards, statuses, fates, fishes, mounts
-# from pymongo.mongo_client import MongoClient
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 def run():
-    # uri = "mongodb+srv://tea_admin:BC5AOSNbRuhJPge4@cluster0.1buu3qz.mongodb.net/?retryWrites=true&w=majority"
+    uri = "mongodb+srv://tea_app:GQzBeQxpwlW3t4mV@cluster0.1buu3qz.mongodb.net/?retryWrites=true&w=majority"
 
-    # # Create a new client and connect to the server
-    # client = MongoClient(uri)
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
 
-    # # Send a ping to confirm a successful connection
-    # try:
-    #     client.admin.command('ping')
-    #     print("Pinged your deployment. You successfully connected to MongoDB!")
-    # except Exception as e:
-    #     print(e)
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
 
     # quests.dump_text_file()
     # cutscenes.dump_text_file()
-    items.dump_text_file()
-    tripletriadcards.dump_text_file()
-    statuses.dump_text_file()
-    fates.dump_text_file()
-    fishes.dump_text_file()
-    mounts.dump_text_file()
+    # items.dump_text_file()
+    # tripletriadcards.dump_text_file()
+    # statuses.dump_text_file()
+    # fates.dump_text_file()
+    # fishes.dump_text_file()
+    # mounts.dump_text_file()
 
