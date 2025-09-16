ea2a7894e69b41dcc2a8fa323b152a28ed44fbf7
Lauren H <naupthia@gmail.com>
2025-09-16 12:40:17 -0400

    update for breaking changes

--- data_export/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
-INPUT_PATH = f"{ROOT_PATH}\\_dumps\\input"
-
-VERSION_NAME = os.listdir(INPUT_PATH)[-1]
-
-DATA_PATH = f"{INPUT_PATH}\\{VERSION_NAME}\\exd"
-OUTPUT_PATH = f"{ROOT_PATH}\\_dumps\\output"
